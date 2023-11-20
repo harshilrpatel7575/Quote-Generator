cde928e4e2bad648e24be1febed2979cc401846c
Harshil Patel <harshilrpatel7575@gmail.com>
2023-11-20 21:32:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vollyapi/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.vollyapi
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.vollyapi.databinding.ActivityMainBinding
+import org.json.JSONArray
+
+class MainActivity : AppCompatActivity() {
+    val url: String ="https://api.quotable.io/quotes/random"
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getMemeData()
+
+        binding.btnNewMeme.setOnClickListener{
+            getMemeData()
+        }
+    }
+
+    private fun getMemeData() {
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                 Log.e("Responce", "getMemeData: " + response.toString())
+                 var jsonArray = JSONArray(response)
+                if (jsonArray.length() > 0) {
+                    val responseObject = jsonArray.getJSONObject(0)
+
+                    val content = responseObject.getString("content")
+                    val author = responseObject.getString("author")
+
+
+
+                    binding.memeTitle.text = content
+                    binding.memeAuther.text = author
+
+
+                } else {
+                    Toast.makeText(this@MainActivity, "No data found", Toast.LENGTH_SHORT).show()
+                }
+
+            },
+            {
+                error->
+
+                Toast.makeText(this@MainActivity, error.localizedMessage,Toast.LENGTH_SHORT).show()
+            })
+
+
+        queue.add(stringRequest)
+    }
+
+}
